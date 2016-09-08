882dc3f4930d6ccd0308dfcf6bde07a0a8f6516d
Peter Strawn <cs50@stfrancishs.org>
2016-09-08 22:03:08 +0000

    Fixed comments on var3

--- unit1/var3.c
+++ unit1/var3.c
@@ -4,13 +4,13 @@
 int main(void)
 {
     // examples of data types
-    char strikeout = 'K';   // 1 byte of memory for a character
+    char strikeout = 'K';               // 1 byte of memory for a character
     string className = "This is CS50."; // 4 or 8 bytes of memory for text
-    int age = 29;           // 4 bytes of memory for an int
+    int age = 29;                       // 4 bytes of memory for an int
     long long anotherAge = 63;          // 8 bytes of memory for an int
-    float pi = 3.14;        // 4 bytes of memory for a real number
+    float pi = 3.14;                    // 4 bytes of memory for a real number
     double morePi = 3.1415962;          // 8 bytes of memory for a real number
-    bool fun = true;        // 1 byte of memory for true/false
+    bool fun = true;                    // 1 byte of memory for true/false
     
     // print-outs of data types
     printf("strikeout: %c\n", strikeout);
