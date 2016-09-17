41bb24e27226f1760fd5d98c9680cb43b56b0624
Peter Strawn <cs50@stfrancishs.org>
2016-09-17 22:33:14 +0000

    Source code for Monday's class

--- chapter1/reviewBad.c
+++ chapter1/reviewBad.c
@@ -7,31 +7,31 @@
 */
 
 #include <stdio.h>
-#include <cs50.h>
+#include  cs50.h
 
 #define COLLEGE 4
 
 int main(void)
 {
-    int gradeLevel;
+    int gradeLevel
     
     do
     {
-        printf("Enter your high school grade level (9-12): ");
-        gradeLevel = GetInt();
-    } while (gradeLevel < 9 || gradeLevel > 12);
+        print("Enter your high school grade level (9-12): ");
+        gradeLevel = getint();
+    } while (gradeLevel < 9 && gradeLevel > 12);
     
     int gradYear;
     
     do
     {
         printf("Enter the last two digits of the year you will be graduating: ");
-        gradYear = GetInt();
-    } while (gradYear < 17 || gradYear > 20);
+        int gradYear = GetInt();
+    } while (gradYear < 17 | gradYear > 20);
     
-    if (gradeLevel == 9 && gradYear != 20)
+    if (gradeLevel == 9 & gradYear != 20)
     {
-        while (gradYear != 20)
+        do (gradYear != 20)
         {
             printf("You don't know when you graduate?!? Try again: ");   
             gradYear = GetInt();
@@ -41,13 +41,13 @@ int main(void)
     {
         while (gradYear != 19)
         {
-            printf("You don't know when you graduate?!? Try again: ");    
+            printf("You don't know when you graduate?!? Try again: ")    
             gradYear = GetInt();
         }
     }
     else if (gradeLevel == 11 && gradYear != 18)
     {
-        while (gradYear != 18)
+        while (gradYear == 18)
         {
             printf("You don't know when you graduate?!? Try again: ");    
             gradYear = GetInt();
@@ -55,17 +55,16 @@ int main(void)
     }
     else if (gradeLevel == 12 && gradYear != 17)
     {
-        while (gradYear != 17)
+        while (gradYear != 17);
         {
             printf("You don't know when you graduate?!? Try again: ");    
-            gradYear = GetInt();
+            gradyear = GetInt();
         }
     }
     
-    for (int i = 0; i < COLLEGE; i++)
+    for (int i = 0, i < COLLEGE, i++)
     {
         printf("You will be a college student for school year 20%d-20%d.\n", gradYear + i, gradYear + i + 1);
     }
     
     printf("Your 25-year high school reunion will be in 20%d, and your 25-year college reunion will be in 20%d.\nStart planning now!\n", gradYear + 25, gradYear + COLLEGE + 25);
-}
