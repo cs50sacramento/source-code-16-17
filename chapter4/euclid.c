1447f04c411085915474575643ce4ce1d7204c81
Peter Strawn <cs50@stfrancishs.org>
2017-01-08 21:57:58 +0000

    Push one final recursion example

---
+++ chapter4/euclid.c
@@ -0,0 +1,35 @@
+/*
+*
+* Find greatest common divisor of two integers
+* Peter Strawn
+* TAOCP - 1.1
+*
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int getGCD(int m, int n)
+{
+    if (n == 0)
+    {
+        return m;
+    }
+    else
+    {
+        return getGCD(n, m % n);
+    }
+}
+
+int main(void)
+{
+    printf("Enter integer one: ");
+    int num1 = GetInt();
+    
+    printf("Enter integer two: ");
+    int num2 = GetInt();
+    
+    int gcd = getGCD(num1, num2);
+    
+    printf("The GCD of %d and %d is %d.\n", num1, num2, gcd);
+}
